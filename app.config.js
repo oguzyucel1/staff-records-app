3eb28e219f027e8ea39c0751d7226ba64af984ad
oguzyucel1 <oguzyucell.oy@gmail.com>
2025-06-20 01:26:30 +0300

    android build config established and added screenshots for readme preview

---
+++ app.config.js
@@ -0,0 +1,63 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "staffapp",
+    slug: "staffapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/appicon.png",
+    scheme: "staffapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+      userInterfaceStyle: "automatic",
+      bundleIdentifier: "com.oguzyucel1.staffapp",
+    },
+
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png", // Arka planı şeffaf olmalı!
+        backgroundColor: "#4c669f",
+      },
+      edgeToEdgeEnabled: true,
+      softwareKeyboardLayoutMode: "pan",
+      permissions: ["android.permission.CAMERA"],
+      package: "com.oguzyucel1.staffapp",
+    },
+
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    splash: {
+      image: "./assets/splash.png",
+      backgroundColor: "#4c669f",
+      resizeMode: "contain",
+    },
+
+    assetBundlePatterns: ["**/*"],
+
+    plugins: ["expo-router"],
+
+    experiments: {
+      typedRoutes: true,
+      tsconfigPaths: true,
+    },
+
+    jsEngine: "hermes",
+
+    extra: {
+      router: {},
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "a854bbcb-c183-43e9-b492-6a5d0bb913dc",
+      },
+    },
+  },
+};
